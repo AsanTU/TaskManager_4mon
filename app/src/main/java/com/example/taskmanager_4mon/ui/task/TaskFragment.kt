faa57ec676329bf91b4d5b8ceaafea34fd0b8533
asant <asantursunaliev0606@gmail.com>
2023-11-24 16:20:13 +0300

    some changes

--- app/src/main/java/com/example/taskmanager_4mon/ui/task/TaskFragment.kt
+++ app/src/main/java/com/example/taskmanager_4mon/ui/task/TaskFragment.kt
@@ -21,7 +21,7 @@ class TaskFragment : Fragment() {
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
-    ): View? {
+    ): View {
         binding = FragmentTaskBinding.inflate(inflater, container, false)
         return binding.root
     }
