68a1c8791a39ff0f32aaaebadfbfcaf35d22ab80
asant <asantursunaliev0606@gmail.com>
2023-12-09 16:34:01 +0300

    hw8

--- app/src/main/java/com/example/taskmanager_4mon/data/local/Pref.kt
+++ app/src/main/java/com/example/taskmanager_4mon/data/local/Pref.kt
@@ -22,7 +22,16 @@ class Pref(private val context: Context) {
         return pref.getString(TEXT_KEY, "")
     }
 
+    fun saveImageUri(imageUri: String){
+        pref.edit().putString(IMAGE_URI_KEY, imageUri).apply()
+    }
+
+    fun getImageUri(): String? {
+        return pref.getString(IMAGE_URI_KEY, "")
+    }
+
     companion object {
+        const val IMAGE_URI_KEY = "image_uri.key"
         const val PREF_NAME = "pref.name"
         const val SHOWED_KEY = "showed.key"
         const val TEXT_KEY = "text.key"
--- app/src/main/java/com/example/taskmanager_4mon/ui/profile/ProfileFragment.kt
+++ app/src/main/java/com/example/taskmanager_4mon/ui/profile/ProfileFragment.kt
@@ -1,12 +1,19 @@
 package com.example.taskmanager_4mon.ui.profile
 
+import android.app.Activity.RESULT_OK
+import android.content.Intent
+import android.net.Uri
 import android.os.Bundle
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.appcompat.app.ActionBar
+import androidx.appcompat.app.AlertDialog
 import androidx.appcompat.app.AppCompatActivity
+import androidx.navigation.fragment.findNavController
+import com.example.taskmanager_4mon.App
+import com.example.taskmanager_4mon.R
 import com.example.taskmanager_4mon.data.local.Pref
 import com.example.taskmanager_4mon.databinding.FragmentProfileBinding
 
@@ -15,6 +22,8 @@ class ProfileFragment : Fragment() {
     private var _binding: FragmentProfileBinding? = null
 
     private val binding get() = _binding!!
+
+    private val PICK_IMAGE_REQUEST = 1
 
     private val pref by lazy {
         Pref(requireContext())
@@ -33,6 +42,39 @@ class ProfileFragment : Fragment() {
         super.onViewCreated(view, savedInstanceState)
         hideActionBar()
         saveData()
+        binding.profileImg.setOnClickListener {
+            pickImageFromGallery()
+        }
+        exitAlertDialog()
+    }
+
+    private fun exitAlertDialog() {
+        binding.ivExit.setOnClickListener {
+            val alertDialogBuilder = AlertDialog.Builder(requireContext())
+                .setTitle(getString(R.string.exit_text))
+                .setMessage(getString(R.string.exit_confirmation))
+                .setPositiveButton(getString(R.string.delete_accept)) { _, _ ->
+                    findNavController().navigate(R.id.phoneFragment)
+                }
+                .setNegativeButton(getString(R.string.cancel)) { _, _ -> }
+            val alertDialog = alertDialogBuilder.create()
+            alertDialog.show()
+        }
+    }
+
+    private fun pickImageFromGallery() {
+        val intent = Intent(Intent.ACTION_PICK)
+        intent.type = "image/*"
+        startActivityForResult(intent, PICK_IMAGE_REQUEST)
+    }
+
+    override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
+        super.onActivityResult(requestCode, resultCode, data)
+        if (requestCode == PICK_IMAGE_REQUEST && resultCode == RESULT_OK && data != null) {
+            val imageUri: Uri? = data.data
+            pref.saveImageUri(imageUri.toString())
+            binding.profileImg.setImageURI(imageUri)
+        }
     }
 
     override fun onDestroyView() {
